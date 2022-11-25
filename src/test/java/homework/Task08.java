a9a90c992d749d12151ab764c9fd830224427ad8
SaimaBokhari <ssbokhari14@gmail.com>
2022-11-25 09:20:13 +0400

    final HW

--- src/test/java/homework/Task08.java
+++ src/test/java/homework/Task08.java
@@ -9,6 +9,7 @@ import test_data.PetStoreTestData;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class Task08 extends PetStoreBaseUrl {
      /*
@@ -37,11 +38,28 @@ public class Task08 extends PetStoreBaseUrl {
 
         // Set the expected data
 
-        PetStoreTestData user1 = new PetStoreTestData(134, "kana yari", "Affaf","Ahmed", "kanayari_rocks@gmail.com", "cokestudio2022", "0123456789", 82);
+        PetStoreTestData user = new PetStoreTestData(134, "kana yari", "Affaf","Ahmed", "kanayari_rocks@gmail.com", "cokestudio2022", "0123456789", 82);
+        Map<String, Object> expectedDataUser = user.expectedDataUserSetUp(134, "kana yari", "Affaf","Ahmed", "kanayari_rocks@gmail.com", "cokestudio2022", "0123456789", 82);
 
         // Send the request and get the response
-        Response response = given(). spec(spec).contentType(ContentType.JSON).body(user1).when().post("/{first}/{second}");
+        Response response = given(). spec(spec).contentType(ContentType.JSON).body(expectedDataUser).when().post("/{first}/{second}");
         response.prettyPrint();
+
+        // Do assertion
+
+        assertEquals(200, response.statusCode());
+
+        /*
+         I got the status code 200 on swagger and on intelliJ as well.. but I don't get to see the body of the data
+         in Json format when I pretty print it. This is the response body I see on the console. I don't know the reason.
+
+        {
+    "code": 200,
+    "type": "unknown",
+    "message": "134"
+}
+
+         */
 
 
 
